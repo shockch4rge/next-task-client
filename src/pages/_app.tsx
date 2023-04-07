364a76613f03a17247c8c06c1fba487886ab9770
Favian <favouriteteo@gmail.com>
2023-04-07 16:52:13 +0800

    feat: set up basic theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
-import { ThemeProvider } from "styled-components";
-import { BaseTheme } from "@lifesg/react-design-system/theme";
-import type { AppProps } from "next/app";
 
+import { ThemeProvider } from "styled-components";
+
+import theme from "@/styles/theme";
+
+import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <ThemeProvider theme={BaseTheme}>
+        <ThemeProvider theme={theme}>
             <Component {...pageProps} />
         </ThemeProvider>
     );
